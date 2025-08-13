966236b008e2099b59c44e3756f36138935fec19
dnyaneshwar suwarnkar <sdnyaneshwar9421@gmail.com>
2025-08-14 02:10:29 +0530

    fix 5

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.URL || "*", 
+  origin: [
+    "https://udyam-registration-phi.vercel.app",
+    "http://localhost:3000"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
